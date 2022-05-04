75dc37d50e4c6ecc00cdd2cae4bc2b135b44f912
abuYahya <abuyahyadiab@gmail.com>
2022-05-04 14:40:16 +0300

    env file

---
+++ app.js
@@ -0,0 +1,73 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const app=express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+console.log(process.env.API_KEY);
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    Email: String,
+    Password: String
+});
+
+
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["Password"]}); //this is must be added before the collection
+
+const User = new mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+})
+
+app.get("/login",function(req,res){
+    res.render("login");
+})
+app.get("/register",function(req,res){
+    res.render("register");
+})
+
+app.post("/register",function(req,res){
+    const newUserData = new User({
+        Email: req.body.username,
+        Password: req.body.password 
+    })
+    
+    newUserData.save(function(err){
+        if(!err)
+            res.render("secrets");
+        else
+            res.send(err);
+    });
+    console.log(newUserData);
+})
+
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({Email: username},function(err,foundUser){
+        console.log(username);
+        console.log(foundUser.Password);
+        if(err)
+            res.send(err);
+        else
+            if(foundUser && foundUser.Password===password)
+                res.render("secrets");
+    });
+});
+
+app.listen(4000,function(){
+    console.log("successfully connected on port 4000");
+});
